98a829312e68928a3ebea111a79489b25c298129
long.qul <long.qul@alibaba-inc.com>
2015-07-29 15:03:44 +0800

    more delegate impl

--- example/simple.js
+++ example/simple.js
@@ -1,5 +1,14 @@
 var githost = require('..');
 
-githost.create().listen(8080, function () {
+var delegate = githost.HostDelegate.create({
+  message: function (event, write, end) {
+    console.log('xxx');
+    write('Powered by GitHost');
+    write('Good bye');
+    end();
+  }
+});
+
+githost.create({delegate: delegate}).listen(8080, function () {
   console.log('git server is up and running at port 8080');
 });
